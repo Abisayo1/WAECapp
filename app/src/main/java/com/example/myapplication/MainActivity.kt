d006a40b17f7354333df39cc0bc43a03f0eef73d
Abisayo <foluagoiabisayo@gmail.com>
2021-06-16 04:46:04 -0700

    Waec App

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,18 +3,36 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.BoringLayout.make
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar()?.hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
 
+        window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
         val button = findViewById<Button>(R.id.button1)
 
         button.setOnClickListener{
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+            if(et_name.text.toString().isEmpty()){
+                Toast.makeText( this,"Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
