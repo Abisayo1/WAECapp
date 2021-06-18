8057fc97f3420a75b545225bce5a89c7de162261
Abisayo <foluagoiabisayo@gmail.com>
2021-06-18 05:04:22 -0700

    Waec App

---
+++ app/src/main/java/com/example/myapplication/BlankActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class BlankActivity : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.activity_main3, container, false)
+}
